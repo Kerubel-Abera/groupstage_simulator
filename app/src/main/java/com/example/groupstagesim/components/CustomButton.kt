e4b494287cd52e5f0523bf1fb17689e220719837
Kerubel Abera <kerubela1@gmail.com>
2023-09-15 10:38:06 +0200

    FIX text overflow on smaller devices

--- app/src/main/java/com/example/groupstagesim/components/CustomButton.kt
+++ app/src/main/java/com/example/groupstagesim/components/CustomButton.kt
@@ -10,6 +10,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 import com.example.groupstagesim.ui.theme.CORNERSHAPE_LARGE
@@ -43,7 +44,10 @@ fun CustomButton(
         Text(
             text = text,
             fontWeight = FontWeight.Bold,
-            fontSize = 36.sp
+            fontSize = 36.sp,
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis,
+            softWrap = true
         )
     }
 }
--- app/src/main/java/com/example/groupstagesim/ui/screens/results/ResultsStandingCard.kt
+++ app/src/main/java/com/example/groupstagesim/ui/screens/results/ResultsStandingCard.kt
@@ -24,6 +24,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -192,6 +193,9 @@ fun LeaderboardText(text: String, modifier: Modifier){
         fontSize = 14.sp,
         color = Color.Black,
         textAlign = TextAlign.Center,
+        maxLines = 1,
+        overflow = TextOverflow.Ellipsis,
+        softWrap = true
     )
 }
 
